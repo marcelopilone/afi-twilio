32bce096a92a41dd77d0b41921a0a5e4e7b76647
marcelopilone <mdpilone@gmail.com>
2021-12-10 09:14:48 -0300

    se agrego descp al SMS

--- Lib/Utility/AfiTwilio.php
+++ Lib/Utility/AfiTwilio.php
@@ -14,38 +14,55 @@ class AfiTwilio {
      * @param  string $usuario
      * @return json in console || false if not send sms
      */
-	public static function enviar_sms( $tokenGenerado,$usuario ) {
+	public static function enviar_sms( $tokenGenerado,$usuario,$peritableUrlId ) {
 		$tokenTwilio   = Configure::read('Afigestion.token_auth_sms'); // Your Auth Token from www.twilio.com/console
 		$tel           = Configure::read('Afigestion.numero_de_telefono_sms');
         $sid           = Configure::read('Afigestion.twilio_sid');
         $emailFalloSms = Configure::read('Afigestion.email_fallo_sms');
-
         $sms = false;
         if( $tel != '0' ){
+
+            $PeritableUrl = ClassRegistry::init('Afigestion.PeritableUrl');
+            $Edificio     = ClassRegistry::init('Afigestion.Edificio');
+            $peritable = $PeritableUrl->find('first',[
+                'contain' => ['PeritableAction'],
+                'conditions' => [
+                    'PeritableUrl.id' => $peritableUrlId
+                ]
+            ]);
+
+            $regionalesConEdificios = [];
+            if( $peritable['PeritableAction']['id'] == AFIGESTION_PERITABLE_ACTION_PADRON_X_EDIFICIO_ID ){
+                $edificiosSelected = json_decode( $peritable['PeritableUrl']['parametros'],true )['edificio_id'];
+                $regionalesConEdificios = $Edificio->dameArrayParaPadronPorEdificio( $edificiosSelected );
+            }
+
+            $regionalText = $edificioText = '';
+            if( !empty( $regionalesConEdificios ) ){
+                    foreach( $regionalesConEdificios['Regional'] as $reg ){
+                        $regionalText.= $reg[0].',';
+                    }
+                    foreach( $regionalesConEdificios['Edificio'] as $edif ){
+                        foreach( $edif as $e ){
+                            $name = (!empty( $e['name'] )) ? $e['name'] : 'Sin nombre';
+                            $calle = (!empty( $e['calle'] )) ? $e['calle'] : 'Sin calle';
+                            $nroCalle = (!empty( $e['numero'] )) ? $e['numero'] : 'Sin numero calle';
+                            $codigo = (!empty( $e['codigo'] )) ? $e['codigo'] : 'Sin codigo';
+                            $nombreCompletoEdificio = $name."-".$calle."-".$nroCalle."(".$codigo.")";
+                            $edificioText.= $nombreCompletoEdificio.',';
+                        }
+                    }
+            }
+            $fecha = "\n Fecha de solicitud de visualización: ".date('d/m/y | h:i:s');
+
             $mensaje = "curl 'https://api.twilio.com/2010-04-01/Accounts/".$sid."/Messages.json' -X POST \
                 --data-urlencode 'From=(205) 619-1827' \
                 --data-urlencode 'To=".$tel."' \
                 --data-urlencode 'MessagingServiceSid=MG5e1f20853d4988ed2253881ee4c1d019' \
-                --data-urlencode 'Body=El usuario ".$usuario." quiere ver un padrón si desea que lo visualicé, darle este token:".$tokenGenerado."' \
-                -u $sid:".$tokenTwilio." > ".WWW_ROOT."sms/resultado.json";
+                --data-urlencode 'Body=El usuario ".$usuario." quiere ver un padrón si desea que lo visualicé, darle este token:".$tokenGenerado." \n ".$regionalText." \n ".$edificioText.$fecha."' \
+                -u $sid:".$tokenTwilio;
             $sms = exec( $mensaje );
-            $file = new File(WWW_ROOT."sms/resultado.json");
-            $json = $file->read(true, 'r');
-            $jsonRespuestaSms = json_decode($json);
-            if( $jsonRespuestaSms->status != 'accepted' ){
-                //enviamos mail si no se envio el sms
-                $Email = new CakeEmail();
-                $Email->viewVars(array(
-                    'usuario' => $usuario,
-                    'tokenGenerado' => $tokenGenerado,
-                ));
-                $Email->template('Twilio.enviar_mail_por_sms_fallido');
-                $Email->emailFormat('html');
-                $Email->from(array(AFIGESTION_EMAIL_SISTEMAS => 'Afigestion'));
-                $Email->to($emailFalloSms);
-                $Email->subject('Token generado para visualizar padrón');
-                $Email->send();
-            }
+            
         }
         echo $sms;
 	}
